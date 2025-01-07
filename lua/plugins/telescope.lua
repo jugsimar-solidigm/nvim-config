98fd779c21e7967fb5727e294b1ce66429d03dce
1Miguel <jmjugsimar@gmail.com>
2025-01-07 23:38:13 +0800

    add some more plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,14 +3,64 @@ return {
 
 	tag = '0.1.8',
 
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{ -- If encountering errors, see telescope-fzf-native README for install instructions
+			"nvim-telescope/telescope-fzf-native.nvim",
+
+		  	-- `build` is used to run some command when the plugin is installed/updated.
+		  	-- This is only run then, not every time Neovim starts up.
+		  	build = "make",
+
+		  	-- `cond` is a condition used to determine whether this plugin should be
+		  	-- installed and loaded.
+		  	cond = function()
+				return vim.fn.executable("make") == 1
+		  	end,
+		},
+		{ "nvim-telescope/telescope-ui-select.nvim" },
+
+		-- Useful for getting pretty icons, but requires a Nerd Font.
+		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+		{ "nvim-telescope/telescope-live-grep-args.nvim" },		
+	},
 
 	config = function()
+		
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 		vim.keymap.set('n', '<leader>fs', builtin.git_files, { desc = 'Telescope git files' })
+		vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Telescope search recent files' })
+		vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Telescope list of normal mode keymap' })
+
+		local telescope = require("telescope")
+		local lga_actions = require("telescope-live-grep-args.actions")
+
+		telescope.load_extension("live_grep_args")
+
+		telescope.setup({
+			extension = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown(),
+				}
+			},
+			live_grep_args = {
+				auto_quoting = true,
+
+				-- define mappings
+				mappings = {
+					i = {
+						["<C-k>"] = lga_actions.quote_prompt(),
+					}
+				},
+			},
+		})
+
+		local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
+		vim.keymap.set("n", "<leader>gc", live_grep_args_shortcuts.grep_word_under_cursor)
+		
 	end
 }
--- lua/remap.lua
+++ lua/remap.lua
@@ -6,6 +6,8 @@ vim.keymap.set("n", "<leader>n", ":bn<cr>")
 vim.keymap.set("n", "<leader>p", ":bp<cr>")
 vim.keymap.set("n", "<leader>x", ":bd<cr>")
 
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
 -- Terminal open bindings
 vim.api.nvim_create_autocmd("TermOpen", {
         group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
